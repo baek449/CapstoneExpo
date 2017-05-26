5730731d58b87730137e1d4b9060a609054b2cb1
imimp1203 <psanha@kaist.ac.kr>
2017-05-26 21:38:22 +0900

    Update Image.js

--- model/Image.js
+++ model/Image.js
@@ -1,32 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var projectSchema = new Schema({
-  projectSummary:{
-	  name:			String,
-	  shortIntro:	String,
-	  status:		String,
-	  startDate:	Date,
-	  endDate:		Date,
-	  field:		[String],
-	  keyword:		[String],
-	  prevProject:	ObjectID,
-	  image:		String,
-	  like:			Number,
-	  view:			Number
-  },
-  affiliation:{
-	  school:		ObjectID,
-	  course:		ObjectID,
-	  semester:		String
-  },
-  members:{
-	  professor:	[mongoose.model('Member').schema],
-	  assistant:	[mongoose.model('Member').schema],
-	  student:		[mongoose.model('Member').schema],
-	  company:		[mongoose.model('Member').schema],
-	  leader:		mongoose.model('Member').schema
-  }
+var imageSchema = new Schema({
+	name:		Stinrg,
+	extension:	String
 });
 
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model('Image', imageSchema);
