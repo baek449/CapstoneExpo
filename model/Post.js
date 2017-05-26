494443610c137948fa81b3d3f53be7677a45e688
imimp1203 <psanha@kaist.ac.kr>
2017-05-26 21:39:02 +0900

    Update Post.js

--- model/Post.js
+++ model/Post.js
@@ -1,32 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var projectSchema = new Schema({
-  projectSummary:{
-	  name:			String,
-	  shortIntro:	String,
-	  status:		String,
-	  startDate:	Date,
-	  endDate:		Date,
-	  field:		[String],
-	  keyword:		[String],
-	  prevProject:	ObjectID,
-	  image:		String,
-	  like:			Number,
-	  view:			Number
-  },
-  affiliation:{
-	  school:		ObjectID,
-	  course:		ObjectID,
-	  semester:		String
-  },
-  members:{
-	  professor:	[mongoose.model('Member').schema],
-	  assistant:	[mongoose.model('Member').schema],
-	  student:		[mongoose.model('Member').schema],
-	  company:		[mongoose.model('Member').schema],
-	  leader:		mongoose.model('Member').schema
+var postSchema = new Schema({
+  
+	  project:		Schema.Types.ObjectId,
+	  title:		String,
+	  contents:		String,
+	  image:		Schema.Types.ObjectId,
+	  writer:		Schema.Types.ObjectId,
+	  writeTime:	Timestamp,
+	  editTime: 	Timestamp,
+	  type:			String,
+	  attend_member	[Schema.Types.ObjectId]
   }
 });
 
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model('Post', PostSchema);
