a78520451d35cf4ba36c0f6680dee7f5a3b12684
jisukang <nalgoshipoo@gmail.com>
2017-05-30 21:05:52 +0900

    jisu

--- router/routerMember.js
+++ router/routerMember.js
@@ -1,7 +1,7 @@
 module.exports = function(app,Model)
 {
 	const Member=Model.Member;
-	const MemberController=require('../controller/postController')(Model);
+	const MemberController=require('../controller/memberController')(Model);
 	// 멤버에 관한 라우터
 	
 	/* 이번에 구현 안 함
@@ -20,7 +20,8 @@ module.exports = function(app,Model)
 	*/
 	app.get('/member/get/:id',function(req,res){
 		// TODO 멤버의 상세 정보를 가져옵니다.
-		PostController.read(req.params.id,function(x){
+		MemberController.get(req.params.id,function(x){
+			console.log(x);
 			// x.result에는 하나의 멤버 정보가 있습니다. 이는 응답으로 넘어가기 전에 Mongoose 객체에서 일반 객체로 바뀌어야 합니다.
 			if(x.result)
 				x.result=x.result.toObject();
